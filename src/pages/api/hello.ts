09290dd77f033407f36a77f8c70cc4d3660a2a43
lehannK <leandro_hollies@hotmail.com>
2023-12-08 17:22:34 -0300

    SSR comparação

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+  timeStamp: Date;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const timeStamp = new Date();
+  res.status(200).json({ name: "John Doe", timeStamp });
 }
